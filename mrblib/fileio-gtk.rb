95242e11407c1b8b7db954bc23e9a6f9e580ef08
masahino <masahino@gmail.com>
2017-03-25 22:49:52 +0900

    improve file-file

--- mrblib/fileio-gtk.rb
+++ mrblib/fileio-gtk.rb
@@ -1,23 +1,8 @@
 module Mrbmacs
   class Application
-    def find_file(filename = nil)
-      filename = @frame.select_file("find file:", @current_buffer.directory)
-      if filename != nil
-        view_win = @frame.view_win
-        @current_buffer.pos = view_win.sci_get_current_pos
-        new_buffer = Buffer.new(filename, @buffer_list.map{|b| b.name})
-        view_win.sci_add_refdocument(@current_buffer.docpointer)
-        view_win.sci_set_docpointer(nil)
-        new_buffer.docpointer = view_win.sci_get_docpointer
-        @buffer_list.push(new_buffer)
-        @prev_buffer = @current_buffer
-        @current_buffer = new_buffer
-        load_file(filename)
-        view_win.sci_set_lexer_language(@current_buffer.mode.name)
-        @current_buffer.mode.set_style(view_win, @theme)
-        view_win.sci_set_sel_back(true, 0xff0000)
-        @frame.set_buffer_name(@current_buffer.name)
-      end
+    def read_file_name(prompt, directory)
+      @frame.select_file(prompt, directory)
     end
+
   end
 end
