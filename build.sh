63c3bb92e773216f94620258378f7a62981bf2e1
masahino <masahino@gmail.com>
2017-11-12 08:19:23 +0900

    use mruby 1.3.0

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 export MRUBY_CONFIG=`pwd`/misc/build_config.rb
 
 if [ ! -d "./mruby/src" ]; then
-  git clone https://github.com/mruby/mruby.git
+  git clone -b 1.3.0 https://github.com/mruby/mruby.git
 fi
 (cd mruby; ./minirake)
 (cd mruby; ./minirake test)
