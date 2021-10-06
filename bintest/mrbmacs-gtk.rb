3f653f59c24d6ce463c76c77848da80e7ba82567
masahino <masahino@gmail.com>
2021-10-06 22:37:15 +0900

    fix path

--- bintest/mrbmacs-gtk.rb
+++ bintest/mrbmacs-gtk.rb
@@ -1,6 +1,6 @@
 $script_dir = File.dirname(__FILE__) + "/scripts/"
 assert('init buffer') do
-  o = `#{ENV['BUILD_BIN_PATH']}#{cmd('mrbmacs-gtk')} -l #{$script_dir}init_buffer`
+  o = `#{cmd('mrbmacs-gtk')} -l #{$script_dir}init_buffer`
   assert_equal 0, $?.exitstatus
   lines = o.split("\n")
   assert_equal "*scratch*", lines[0]
